304413a2096ff57b35781183c92d9f9e7238720a
Bahia <haikal.bahia03@gmail.com>
2023-04-25 14:29:39 -0700

    DONE

--- _printf.c
+++ _printf.c
@@ -15,16 +15,16 @@ int _printf (const char *format, ...)
 
         va_start(ap, format);
 
-        if (!format || (format [0]== '%' && !format[1]))
+        if (!format || (format[0] == '%' && !format[1]))
         return (-1); 
-        if (format[0]== '%' && !format [1]==' ' && !format [2])
+        if (format[0] == '%' && format[1] == ' ' && !format[2])
         return (-1);
         for (p = (char *)format; *p; p++)
         {
                 init_params (&params, ap);
-                if (*p = '%')
+                if (*p != '%')
                 {
-                        sum += _putchar (*p);
+                        sum += _putchar(*p);
                         continue;
                 }
                 start = p;
@@ -37,12 +37,12 @@ int _printf (const char *format, ...)
                 p = get_precision(p, &params, ap);
                 if (get_modifier(p, &params))
                         p++;
-                if (!get specifier(p))
-                        sum += print_from_to (start, p,params.1_modifier || params.h_modifier ? p 1 : 0);
+                if (!get_specifier(p))
+                        sum += print_from_to(start, p, params.l_modifier || params.h_modifier ? p - 1 : 0);
                 else
-                        sum += get_print_func (p, ap, &params);
+                        sum += get_print_func(p, ap, &params);
         }
-        _putchar (BUF_FLUSH);
+        _putchar(BUF_FLUSH);
         va_end(ap);
         return (sum);
 }
